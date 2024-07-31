067a06ea684bbca4b3b11d5e8c21eb1cba9f0a69
Sourav Basak <110307917+souravbsk@users.noreply.github.com>
2024-08-01 03:54:14 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set the correct path for Node.js
+export PATH=/root/.nvm/versions/node/v22.5.1/bin:$PATH
+
 # Navigate to the project directory
 cd /root/test-applicaion/ph-testy-app/ph-testy-treat-client/
 
@@ -8,7 +11,3 @@ npm install
 
 # Build the project
 npm run build
-
-# Optionally, restart services or perform other deployment tasks
-# Example: Restart a service if using pm2
-# pm2 restart all
